69075904808a36367c139ae50db28f06c71b2319
Grishma Ptael <patelgrishma9911@gmail.com>
2019-11-11 20:22:18 -0500

    deployment

--- server.js
+++ server.js
@@ -124,6 +124,6 @@ app.post("/articles/:id", function(req, res) {
 });
 
 // Start the server
-app.listen(process.env.PORTv|| PORT, function() {
+app.listen(process.env.PORT|| PORT, function() {
   console.log("App running on port " + PORT + "!");
 });
